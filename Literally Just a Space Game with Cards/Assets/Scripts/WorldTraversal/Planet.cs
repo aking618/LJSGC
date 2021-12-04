e86d999294463880dc1ef76eae39436219c9444a
Dillon Williams <dillonwilliams2001@yahoo.com>
2021-12-03 22:30:57 -0600

    Added cool planet hover visual thing

--- Literally Just a Space Game with Cards/Assets/Scripts/WorldTraversal/Planet.cs
+++ Literally Just a Space Game with Cards/Assets/Scripts/WorldTraversal/Planet.cs
@@ -5,8 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class Planet : MonoBehaviour
 {
-
     public bool isEnabled = false;
+    public bool isHover = false;
+
+    public Vector3 originalPosition;
+
+    public void Start()
+    {
+        originalPosition = transform.position;
+    }
+
+    public void Update()
+    {
+        if(transform.position.y > originalPosition.y + 0.2)
+        {
+            return;
+        }
+        if(isHover)
+        {
+             transform.Translate(Vector3.up * Time.deltaTime * 0.5f);
+        }
+    }
 
     private void OnMouseDown()
     {
@@ -15,5 +34,16 @@ public class Planet : MonoBehaviour
         {
             SceneManager.LoadScene("BattleScene");
         }
+    }
+    
+    private void OnMouseEnter()
+    {
+        isHover = true;
+    }
+
+    private void OnMouseExit()
+    {
+        transform.position = originalPosition;
+        isHover = false;
     }
 }
