078f1838473cb40a99b6a95f23d8bf0c6abfff49
Dillon Williams <dillonwilliams2001@yahoo.com>
2021-12-03 22:55:42 -0600

    Added dynamic card loading

--- Literally Just a Space Game with Cards/Assets/Scripts/Card/CardArena.cs
+++ Literally Just a Space Game with Cards/Assets/Scripts/Card/CardArena.cs
@@ -8,9 +8,18 @@ public class CardArena : MonoBehaviour
     public List<GameObject> hand = new List<GameObject>(); // holds all of the cards in our hand
     public List<GameObject> cardSlots = new List<GameObject>(); // holds and displays the card on the field of play
 
+    public List<GameObject> Deck = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
+        //Generate Cards in Hand randomly from Deck of Cards
+        for(int i = 0; i < cardSlots.Count; i++) // We only hold the number of card slots that exist in our hand
+        {
+            int cardInDeck = Random.Range(0, Deck.Count - 1);
+            hand.Add(Deck[cardInDeck]);
+        }
+
         int slotIndex = 0;
         foreach(GameObject card in hand) // goto the next card in our hand
         {
---
+++ Literally Just a Space Game with Cards/Assets/Scripts/WorldTraversal/readme.txt
@@ -0,0 +1,5 @@
+Ayren King and Dillon Williams Advanced Techniques Attempt:
+
+World Level System:
+- The world level system works as intended but when invisioned, we wanted it to be procedurally generated and allow the player to travel a vast system on their own accord; however, due to time limitations as well as intense complexity this system couldn't be fully fleshed out and had to settle for the level system as it is now.
+
